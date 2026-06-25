e060b8ae85659e22870ae9f2fc14c7731fbf9dcc
Tyoo18 <aldo.rahmadan59@smk.belajar.id>
2026-06-25 18:55:08 +0700

    add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://syntaxpad.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://syntaxpad.vercel.app/readme-generator",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://syntaxpad.vercel.app/env-boilerplate",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://syntaxpad.vercel.app/conventional-commit",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
